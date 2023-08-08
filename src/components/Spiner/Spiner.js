4578f62f66bffc7e7ea4a0df0f3c543469dde824
Andriy_Pereginets <pereginets1995@gmail.com>
2023-08-08 14:05:27 +0300

    first commit

---
+++ src/components/Spiner/Spiner.js
@@ -0,0 +1,122 @@
+
+
+const Spiner = () => {
+    return <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" style={{margin: 'auto', background: 'none', display: 'block', shapeRendering: 'auto'}} 
+    width="190px" height="190px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+    <g transform="rotate(0 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.7969303423848879s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(12.857142857142858 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.7674144037780403s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(25.714285714285715 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.7378984651711925s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(38.57142857142857 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.7083825265643449s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(51.42857142857143 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.6788665879574971s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(64.28571428571429 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.6493506493506495s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(77.14285714285714 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.6198347107438017s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(90 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.590318772136954s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(102.85714285714286 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.5608028335301063s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(115.71428571428571 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.5312868949232586s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(128.57142857142858 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.5017709563164109s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(141.42857142857142 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.47225501770956324s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(154.28571428571428 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.44273907910271554s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(167.14285714285714 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.41322314049586784s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(180 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.38370720188902013s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(192.85714285714286 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.35419126328217243s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(205.71428571428572 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.32467532467532473s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(218.57142857142858 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.295159386068477s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(231.42857142857142 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.2656434474616293s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(244.28571428571428 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.23612750885478162s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(257.14285714285717 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.20661157024793392s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(270 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.17709563164108622s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(282.85714285714283 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.1475796930342385s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(295.7142857142857 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.11806375442739081s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(308.57142857142856 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.08854781582054311s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(321.42857142857144 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.059031877213695405s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(334.2857142857143 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="-0.029515938606847703s" repeatCount="indefinite"></animate>
+      </rect>
+    </g><g transform="rotate(347.14285714285717 50 50)">
+      <rect x="48.5" y="24.5" rx="1.5" ry="2.5" width="3" height="5" fill="#690808">
+        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="0.8264462809917356s" begin="0s" repeatCount="indefinite"></animate>
+      </rect>
+    </g>
+    </svg>
+}
+
+export default Spiner;
---
+++ src/components/charList/CharList.js
@@ -0,0 +1,111 @@
+import { Component } from 'react/cjs/react.production.min';
+import MarvelService from '../../services/MarvelService';
+import Spiner from '../Spiner/Spiner';
+
+import './charList.scss';
+
+class CharList extends Component {
+    state = {
+        randomCharacters: [],
+        loading: true, 
+        error: false,
+    }
+
+    marvelService = new MarvelService();
+
+      onError = () => {
+        this.setState({
+            loading: false,
+            error: true
+        })
+    }
+
+    onCharLoaded = () => {
+        this.setState({
+            loading: false
+        }) 
+    }
+
+    componentDidMount= () => {
+        this.updateCharsList();
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+
+    generateRandomId = () => {
+        const min = 1011000;
+        const max = 1011400;
+        return Math.floor(Math.random() * (max - min + 1) + min);
+      };
+
+      getFullList = () => {
+        const numberOfCharacters = 9;
+        const charactersPromises = [];
+    
+        for (let i = 0; i < numberOfCharacters; i++) {
+          const id = this.generateRandomId();
+          const charPromise = this.marvelService.getCharacter(id);
+          charactersPromises.push(charPromise);
+        }
+    
+        return Promise.all(charactersPromises);
+      };
+
+
+    updateCharsList = () => {
+        this.setState({
+        loading: true, 
+        error: false})
+        this.getFullList()
+            .then((char) => {
+                this.setState({
+                randomCharacters: char,
+                loading: false
+                });
+            })
+            .then(this.onCharLoaded)
+            .catch(this.onError);
+        }
+
+    
+    render() {
+        const  {randomCharacters, loading} = this.state;
+        const spiner = loading ? <Spiner/> : null;
+
+
+
+        const charactersList = randomCharacters.map((char, index) => {
+            let imgStyle = {objectFit: 'cover'};
+            if (char.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
+            imgStyle = {objectFit: 'contain'};
+            }; 
+
+            if (loading) {
+                return <li style={{backgroundColor: 'white', display: 'flex', alignItems: 'center'}} key={index} className="char__item">
+                {spiner}
+            </li>
+            } else {
+                return  <li key={index} className="char__item">
+                            <img style={imgStyle} src={char.thumbnail} alt={char.name}/>
+                            <div className="char__name">{char.name}</div>
+                        </li>
+            }
+
+           
+        })
+        return (
+            <div className="char__list">
+                <ul className="char__grid">
+                    {charactersList}
+                </ul>
+                <button className="button button__main button__long">
+                    <div className="inner" onClick={this.updateCharsList}>load more</div>
+                </button>
+            </div>
+        )
+    }
+}
+
+export default CharList;
